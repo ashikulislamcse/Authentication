6df4b84211219ab472b25208255d4c2fd2b871a0
ashikulislamcse <ashikulislamcse@gmail.com>
2025-03-13 13:58:51 +0600

    Login Complete

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import User from "../Models/userModel.js";
+import jwt from "jsonwebtoken";
 
 export const Register = async (req, res) => {
   const { name, email, password } = req.body;
@@ -27,7 +28,40 @@ export const Register = async (req, res) => {
 };
 
 export const Login = async (req, res) => {
-  res.send("Login route");
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid Password" });
+    }
+    const payload = {
+      user: {
+        id: user._id,
+        email: user.email,
+      },
+    };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.status(200).json({
+      message: "Login successful",
+      token: "Barear "+ token,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
 };
 
 export const Profile = async (req, res) => {
